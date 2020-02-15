e7cf2c20040f6c6aaf703c4b1bcee1163c7e5064
gzavaglia <gio.zavaglia@gmail.com>
2020-02-15 14:41:46 -0600

    Done.

---
+++ src/reducers/root.js
@@ -0,0 +1,12 @@
+  
+import authorsReducer from './authors'
+import booksReducer from './books'
+import { combineReducers } from "redux";
+
+
+const rootReducer = combineReducers({
+  authors: authorsReducer,
+  books: booksReducer
+});
+ 
+export default rootReducer;
